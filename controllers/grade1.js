0da8cc1aa2d34824943646c0f814fc80d653e0a3
amitdoddamani123 <amit.rcb95@gmail.com>
2023-11-14 18:45:10 -0600

    added create files

--- controllers/grade1.js
+++ controllers/grade1.js
@@ -14,4 +14,18 @@ exports.grade_view_one_Page = async function(req, res) {
     }
 };
 
+// Handle building the view for creating a grade.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.grade_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('gradescreate', { title: 'grade Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
 
--- routes/grades1.js
+++ routes/grades1.js
@@ -3,6 +3,7 @@ const grade_controlers= require('../controllers/grade1');
 var router = express.Router();
 
 router.get('/detail', grade_controlers.grade_view_one_Page);
-
+/* GET create grade page */
+router.get('/create', grade_controlers.grade_create_Page);
 
 module.exports = router;
