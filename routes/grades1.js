87b8e956175ee6c8384aea01f98ed6576da3d7c1
amitdoddamani123 <amit.rcb95@gmail.com>
2023-11-28 18:52:36 -0600

    modified files for update login

--- routes/grades1.js
+++ routes/grades1.js
@@ -2,12 +2,26 @@ var express = require('express');
 const grade_controlers= require('../controllers/grade1');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
+
 router.get('/detail', grade_controlers.grade_view_one_Page);
 /* GET create grade page */
 router.get('/create', grade_controlers.grade_create_Page);
 /* GET create update page */
-router.get('/update', grade_controlers.grade_update_Page);
+router.get('/update',secured, grade_controlers.grade_update_Page);
 /* GET delete grade page */
 router.get('/delete', grade_controlers.grade_delete_Page);
+/* router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });   */
 
 module.exports = router;
